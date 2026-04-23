d2d1d1698bbe4bfd6e83fede6075b42acfff0ade
Saviorov <saviorov@yandex.ru>
2026-04-23 13:58:15 +0300

    Configure GitHub Pages deployment

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ import SearchPage from "../pages/SearchPage.vue";
 import MovieDetailsPage from "../pages/MovieDetailsPage.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
